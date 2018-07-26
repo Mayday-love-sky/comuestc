51f6180676d1aa023a260884c6812a7bcc40dc7d
Antionia_Mao <867961557@qq.com>
2018-07-26 14:46:05 +0800

    修改测试

--- src/main/java/uestc/pojo/statsAccount.java
+++ src/main/java/uestc/pojo/statsAccount.java
@@ -1,10 +1,16 @@
 package uestc.pojo;
 
 public class statsAccount {
+    //数据非法
     public static int INVALID_DATA=101;
+    //账户已存在
     public static int ACCOUNT_EXISTS=100;
+    //注册成功
     public static int REGISTER_SUCCESS=1;
+    //账户不存在
     public static int ACCOUNT_UNEXISTS=103;
+    //登陆成功
     public static int LOGIN_SUCCESS=1000;
+    //密码错误
     public static int PASSWORD_WRONG=102;
 }
